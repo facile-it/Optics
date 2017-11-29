7ad5ecc4db53d78693df023a8655fd333e996bb2
Elviro Rocca <elviro.rocca@facile.it>
2017-11-29 15:34:39 +0100

    FPI-20 added comment on Affine laws

--- Sources/Affine.swift
+++ Sources/Affine.swift
@@ -121,6 +121,12 @@ extension Array {
 
 // MARK: - Affine Laws
 
+/*:
+ ## Enforcing Affine laws
+ 
+ Affine laws are very similar to Lens laws, with the exception that setting and getting is going to be verified only if the "try" passes, that is, the functions don't return nil.
+ :*/
+
 public enum AffineLaw {
 	public static func trySetTryGet<Whole, Part, SomeAffine>(affine: SomeAffine, whole: Whole, part: Part) -> Bool where Part: Equatable, SomeAffine: AffineType, SomeAffine.SType == Whole, SomeAffine.TType == Whole, SomeAffine.AType == Part, SomeAffine.BType == Part {
 		guard let newWhole = affine.trySet(part)(whole) else { return true }
